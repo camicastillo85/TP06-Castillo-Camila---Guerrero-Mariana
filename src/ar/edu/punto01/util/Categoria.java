f67ce3bf0719aecdff68620920361f7dcce68482
Camila Castillo <Camila Castillo@DESKTOP-OMC47CR>
2024-09-23 21:47:33 -0300

    Inicializacion del proyecto Desarrollo del punto 1 y 4 Castillo Camila

---
+++ src/ar/edu/punto01/util/Categoria.java
@@ -0,0 +1,8 @@
+package ar.edu.punto01.util;
+
+public enum Categoria {
+	TELEFONIA, 
+	INFORMATICA, 
+	ELECTROHOGAR,
+    HERRAMIENTAS
+}
---
+++ src/ar/edu/punto01/util/Origen.java
@@ -0,0 +1,8 @@
+package ar.edu.punto01.util;
+
+public enum Origen {
+	ARGENTINA,
+	CHINA,
+	BRASIL,
+	URUGUAY
+}
